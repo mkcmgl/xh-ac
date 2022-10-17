270f4bf9218ea3d016be8a72806d33188545a9bc
mkcmgl <2515318247@qq.com>
2022-10-17 14:42:55 +0800

    auth-topall

--- src/views/auth/index.vue
+++ src/views/auth/index.vue
@@ -3,24 +3,32 @@
         <div class="title">实名认证</div>
 
         <div class="top">
-            <div class="first">
-                <img src="@/assets/images/a170.png" alt="">
-                <span class="back">1</span>
-                <span class="dsc">授权数字身份标识</span>
+            <div class="topNum">
+                <div class="first">
+                    <img src="@/assets/images/a170.png" alt="">
+                    <span class="back">1</span>
+                
+                    <div class="dscMiddle">授权数字身份标识</div>
+                </div>
+                <i class="middle"></i>
+                <div class="first">
+                    <img src="@/assets/images/auth169.png" alt="">
+                    <span class="back">2</span>
+                    <div class="dscMiddle">填写申请材料</div>
+                </div>
+                <i class="middle"></i>
+                <div class="first">
+                    <img src="@/assets/images/auth169.png" alt="">
+                    <span class="back">3</span>
+                    <div class="dscMiddle">完成认证申请</div>
+                </div>
             </div>
-            <i class="first"></i>
-            <div class="second">
-                <img src="@/assets/images/auth169.png" alt="">
-                <span class="back">2</span>
-                <span class="dsc">填写申请材料</span>
-            </div>
-            <i class="first"></i>
-            <div class="third">
-                <img src="@/assets/images/auth169.png" alt="">
-                <span class="back">3</span>
-                <span class="dsc">完成认证申请</span>
+            <div class="dec">
+           
+ 
             </div>
         </div>
+       
         <div class="centerMain"></div>
     </div>
 </template>
@@ -48,7 +56,44 @@ export default {
 <style lang="scss" scoped>
 @import "~@/assets/styles/public.scss";
 
+.top{
+    padding-left: 14.75rem;
+    padding-top: 3.125rem;
+    padding-right: 14.75rem;
+    .topNum{
+        display: flex;
+        justify-content: space-between;
+        .first{
+     
+            position: relative;
+            justify-content: space-between;
+            text-align: center;
+        }
+        .first img {
+            height: 6.25rem;
+        }
+        .first .back {
+            position: relative;
+            top: -4.625rem;
+            font-size: 2rem;
+            font-family: PingFang SC-Regular, PingFang SC;
+            font-weight: 400;
+            color: #FFFFFF;
+            display: block;
+        }
+        .dscMiddle {
+            margin-top: -1.25rem;
+        }
+        .middle {
+            display: block;
+            height: 3.75rem;
+            width: 12.5rem;
+            border-bottom: 0.1875rem solid #dfecf7 ;
+            padding: 1rem 0;
+        }
+    }
 
+}
 
 
 </style>
