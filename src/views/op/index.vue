3a91b8419d83b0a9197707095b6ffe6f7a1b292e
mkcmgl <2515318247@qq.com>
2022-11-04 17:23:03 +0800

    op

---
+++ src/views/op/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "XhAcIndex",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
